37580fc8a376dee550a17ac1ec51d5340b55e18f
Mahesh <mahesh@beehively.com>
2017-11-10 18:17:28 +0530

    homepage to point custom domain

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  async handleClick() {
+    const { default: component } = await import("./Home");
+    this.setState({
+      component: component
+    });
+  }
   render() {
+    const C = this.state.component;
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +25,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleClick}> Get component </button>
+        { C ? <C /> : null }
       </div>
     );
   }
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AsyncComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h4> This is async Component </h4>
+      </div>
+    );
+  }
+}
+
+export default AsyncComponent;
