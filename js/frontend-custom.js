407ce2f485079ceace52caa88d8ba4ff23dfbf93
Mahesh <thumarmahesh@gmail.com>
2017-11-11 09:04:26 +0530

    Updates

---
+++ js/frontend-custom.js
@@ -0,0 +1,1 @@
+jQuery('.gallery-item').magnificPopup({type:'image',gallery:{enabled:true}});jQuery('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({disableOn:700,type:'iframe',mainClass:'mfp-fade',removalDelay:160,preloader:false,fixedContentPos:false});jQuery('.single-item').magnificPopup({type:'image',});
